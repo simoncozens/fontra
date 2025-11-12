a68a47541d492174e5b3894b63227b9a0446be9e
Simon Cozens <simon@simon-cozens.org>
2025-11-12 10:59:33 +0000

    Set backend from localstorage

--- src-js/views-editor/src/start.js
+++ src-js/views-editor/src/start.js
@@ -1,8 +1,12 @@
+import { setBackend } from "@fontra/core/backend-api.js";
 import "@fontra/core/theme-settings.js";
 
 import { EditorController } from "@fontra/views-editor/editor.js";
 
 async function startApp() {
+  const backend = localStorage.getItem("fontraBackend") || "python";
+
+  setBackend(backend);
   window.editorController = await EditorController.fromBackend();
 }
 
--- src-js/views-fontinfo/src/start.js
+++ src-js/views-fontinfo/src/start.js
@@ -1,8 +1,12 @@
+import { setBackend } from "@fontra/core/backend-api.js";
 import "@fontra/core/theme-settings.js";
 
 import { FontInfoController } from "@fontra/views-fontinfo/fontinfo.js";
 
 async function startApp() {
+  const backend = localStorage.getItem("fontraBackend") || "python";
+
+  setBackend(backend);
   window.fontInfoController = await FontInfoController.fromBackend();
 }
 
